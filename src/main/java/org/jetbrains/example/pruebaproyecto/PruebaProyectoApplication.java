2fd1237bc42597720be3af77151037d60b1d58c3
mauricio <mauricio.chaves.chaves@est.una.ac.cr>
2024-03-25 19:14:31 -0600

    primer commit

---
+++ src/main/java/org/jetbrains/example/pruebaproyecto/PruebaProyectoApplication.java
@@ -0,0 +1,13 @@
+package org.jetbrains.example.pruebaproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaProyectoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaProyectoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jetbrains/example/pruebaproyecto/PruebaProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jetbrains.example.pruebaproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaProyectoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
